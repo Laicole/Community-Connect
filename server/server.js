6ef4b919c6346e9828cc9881baf7a486f919f0cd
LaiCole Dobie <laicoledobie@ymail.com>
2026-08-08 22:29:29 -0500

    Added MongoDB Connection setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
