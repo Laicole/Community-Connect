9e24d240a2b3cef2a3f117e90bf21f81e03a0432
LaiCole Dobie <laicoledobie@ymail.com>
2026-08-09 23:27:32 -0500

    Add secure user registration with bcrypt

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,5 @@
-import User from"../models/user.js";
-import bcrpyt from "bcryptjs";
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 export const registerUser = async (req, res) => {
   try {
@@ -10,34 +10,40 @@ export const registerUser = async (req, res) => {
         message: "Name, email, and password are required"
       });
     }
-    
+
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({
+        message: "User already exists"
+      });
     }
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        ageGroup,
-        interests
-    }); 
+    // Encrypt/hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    return res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        ageGroup: user.ageGroup,
-        interests: user.interests,
-        favorites: user.favorites,
-        createdAt: user.createdAt,
+    // Save user to MongoDB
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      ageGroup,
+      interests
     });
 
- } catch (error) {
-      return res.status(400).json({ message: error.message 
-
+    // Send user information back WITHOUT password
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      ageGroup: user.ageGroup,
+      interests: user.interests,
+      favorites: user.favorites,
+      createdAt: user.createdAt
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message
     });
   }
 };
