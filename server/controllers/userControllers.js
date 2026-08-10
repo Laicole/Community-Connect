4e0f9a8f9dce4a85fa27e2a7bc7b26f333df53f4
LaiCole Dobie <laicoledobie@ymail.com>
2026-08-09 23:15:56 -0500

    correcting typos in code that are affecting the functionality of the code. The following files have been modified:

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -13,6 +13,10 @@ export const registerUser = async (req, res) => {
     
     const existingUser = await User.findOne({ email });
 
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const user = await User.create({
         name,
         email,
